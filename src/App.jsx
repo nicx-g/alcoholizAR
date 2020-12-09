ee54ea30ed8ce7c5a0d7a5dcd26ef6770b366ec5
Nicolás Gomez <nicx_g@hotmail.com>
2020-12-08 23:44:57 -0300

    Cree el nav de la pag

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import Logo from './resources/images/logo-icon.png';
+import './styles/main.scss'
+
+import Nav from './components/global/nav/Nav.js'
 
 function App() {
     return (
-		<div style={{height: 100 + "vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", background: "linear-gradient(45deg, #e66465, #9198e5)"}}>
-			<h1 style={{textAlign: "center", color: "white", fontWeight: "900", fontSize: 2 + "rem"}}>Probablemente haga una página de alcohol. Será lo más ético? Quién sabe</h1>
-			<img src={Logo} alt="Este va a ser temporalmente el logo de la pag" width="300" />
-
-			<div>
-				<h2 style={{textAlign: "center", color: "white", fontWeight: "900", fontSize: 2 + "rem"}}>Estoy intentando encontrar extensiones para escribir en jsx con la facilidad que tengo en html pero ta dificil. Será momento de escribir mis propias extensiones? </h2>
-				<h2 style={{textAlign: "center", color: "white", fontWeight: "900", fontSize: 2 + "rem"}}>Todo esto es de prueba, por supuesto.</h2>
-			</div>
-			
-		</div>
+		<Nav/>
     );
 }
 
